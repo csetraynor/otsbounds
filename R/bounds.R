054fb626b3e877950b0befd91b6291a3d8f472d8
csetraynor <carlos.serratraynor@astrazeneca.com>
2022-02-22 11:13:25 +0000

    add na bound

--- R/bounds.R
+++ R/bounds.R
@@ -1,18 +1,41 @@
 #' Calculate OTS bounds
 #' Main function of \code{otsbounds}. Calculates Optimal Treatment Selection bounds
 #' @param ps prognostic score
-#' @param obs Observed effect, e.g. observed mean
-#' @param a The lowest bound for the potential outcome
+#' @param y1 Observed effect on treatment 1
+#' @param y0 Observed effect on treatment 0
+#' @param beta The lowest bound for the potential outcome
+#' @param ... ignored
 #' @export
 #' @examples
 #' ps = c(0.1, 0.2, 0.1, 0.4)
 #' obs = data.frame(a1 = c(1, 2, 3, 4), a2 = c(4, 5, 6, 7))
 #' a = 0
-ots_bound <- function(ps, obs, a) {
+ots_bound <- function(ps, y1, y0, beta, ...) {
+
+  UB = ps * y1 - ps * beta
+  LB = (1 - ps) * beta - (1 - ps) * y0
+
+  return(data.frame(UB = UB, LB = LB))
+
+}
 
 
-  UB = ps*obs[1,,drop = T] - ps * a
-  LB = (1-ps) * a - (1 - ps) * obs[2,,drop = T]
+#' Calculate No Assumptions bounds
+#' Main function of \code{otsbounds}. Calculates Optimal Treatment Selection bounds
+#' @param ps prognostic score
+#' @param y1 Observed effect on treatment 1
+#' @param y0 Observed effect on treatment 0
+#' @param beta The lowest possible potential outcome
+#' @param alpha The highest possible potential outcome
+#' @export
+#' @examples
+#' ps = c(0.1, 0.2, 0.1, 0.4)
+#' obs = data.frame(a1 = c(1, 2, 3, 4), a2 = c(4, 5, 6, 7))
+#' a = 0
+no_assumption_bound <- function(ps, y1, y0, beta, alpha) {
+
+  UB = ps * y1 - ps * beta
+  LB = (1 - ps) * beta - (1 - ps) * y0
 
   return(data.frame(UB = UB, LB = LB))
 
